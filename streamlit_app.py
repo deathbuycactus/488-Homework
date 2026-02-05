2f0a8a56350ab470dbce43de6dde431ea3478a60
deathbuycactus <131403532+deathbuycactus@users.noreply.github.com>
2026-02-05 04:03:58 +0000

    hw 3 deafult, lab 3 copy/paste

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import streamlit as st
 pg = st.navigation({
     "HW Manager": [
         st.Page("HW/HW1.py", title="HW 1"),
-        st.Page("HW/HW2.py", title="HW 2", default=True),
-        st.Page("HW/HW3.py", title="HW 3"),
+        st.Page("HW/HW2.py", title="HW 2"),
+        st.Page("HW/HW3.py", title="HW 3", default=True),
         st.Page("HW/HW4.py", title="HW 4"),
         st.Page("HW/HW5.py", title="HW 5"),
         st.Page("HW/HW6.py", title="HW 6"),
